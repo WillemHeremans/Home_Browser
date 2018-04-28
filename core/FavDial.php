d6b9781a61ef12eb8da4ebdae5cef7f0bbf57d2f
WillemHeremans <willemheremans@gmail.com>
2018-04-29 00:50:05 +0200

    Add category, Add icon, Modal box

--- core/FavDial.php
+++ core/FavDial.php
@@ -39,9 +39,31 @@
 				<div class="rang">
 
 					<div class="content">
-					<a title="Add content..." href="#"><img src="./img/add.svg" /></a>
+					<a title="Add content..." href="#add_item"><img src="./img/add.svg" /></a>
 					<p>Add</p>
-
+					</div>
+					
+					<div class="modalLayer" id="add_item">
+					<div class="popup_block">
+					
+					<a href="#home" class="croix">&#10006;</a>
+					<form action="" method="post" id="test">
+		
+						  			<label>Title:</label>
+						  			<input type="text" name="title"></input><br />
+						 
+						  			<label>Description:</label>
+						  			<input type="text" name="description"></input><br />
+						  			
+						  			<label>url:</label>
+						  			<input type="text" name="url"></input><br />
+						  			
+						  			<label>Icone:</label>
+						  			<input type="text" name="img"></input><br />
+						  			
+						  			<input type="submit" />
+						  			</form>
+					</div>
 					</div>
 
 				</div>';
@@ -49,7 +71,7 @@
 				fwrite($save, $dial);
 				fclose($save);
 			
-				header('Location: index.php');
+				header('Location: index.php#home');
 		
 	
 	
