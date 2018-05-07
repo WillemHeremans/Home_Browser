6893cc0c2ffdc3876578cfffa71fe80e3c280063
willemHeremans <willemheremans@gmail.com>
2018-05-07 15:34:02 +0200

    fix dialname special characters

--- core/UpdateDial.php
+++ core/UpdateDial.php
@@ -9,11 +9,11 @@
 			$oldname = $_POST['dial_name'];
 			$newname = $_POST['update_dial'];
 			
-			$str=file_get_contents('./include/'.$oldname.'.php');
-			$str=str_replace('$dialename = "'.$oldname.'"', '$dialename = "'.$newname.'"',$str);
-			file_put_contents('./include/'.$oldname.'.php', $str);
+			$str=file_get_contents("./include/$oldname.php");
+			$str=str_replace("$dialename = ".$oldname."", "$dialename = ".$newname."",$str);
+			file_put_contents("./include/$oldname.php", $str);
 			
-			rename ('./include/'.$oldname.'.php', './include/'.$newname.'.php');
+			rename ("./include/$oldname.php", "./include/$newname.php");
 			
 			header('Location: index.php#home');
 						
