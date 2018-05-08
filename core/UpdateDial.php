c2ee6392c4f01940431418849a0db118136df1b8
willemHeremans <willemheremans@gmail.com>
2018-05-08 15:29:39 +0200

    fix: rename or delete dial = OK

--- core/UpdateDial.php
+++ core/UpdateDial.php
@@ -9,14 +9,17 @@
 			$oldname = $_POST['dial_name'];
 			$newname = $_POST['update_dial'];
 			
+			$adapt = str_replace('/', '_(_', $oldname);
+			$adapt = str_replace('\\', '_)_', $adapt);
+			
 			$rename = str_replace('/', '_(_', $newname);
 			$rename = str_replace('\\', '_)_', $rename);
 			
-			$str=file_get_contents('./include/'.$oldname.'.php');
+			$str=file_get_contents('./include/'.$adapt.'.php');
 			$str=str_replace('$dialename = "'.$oldname.'"', '$dialename = "'.$newname.'"',$str);
-			file_put_contents('./include/'.$oldname.'.php', $str);
+			file_put_contents('./include/'.$adapt.'.php', $str);
 			
-			rename ("./include/$oldname.php", "./include/$rename.php");
+			rename ("./include/$adapt.php", "./include/$rename.php");
 			
 			header('Location: index.php#home');
 						
