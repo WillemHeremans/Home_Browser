6dc424e49591bc0b6986b5ee4dbe6a2cf8e62f56
willemHeremans <willemheremans@gmail.com>
2018-05-08 11:18:28 +0200

    improving code, adding comments

--- core/DeleteDial.php
+++ core/DeleteDial.php
@@ -6,32 +6,33 @@
 		function delete ()
 		
 		{
+			//Connection to dial's db:
 			try
-				{
-				$pdo = new PDO('sqlite:'.dirname(__DIR__).'/db/home.sqlite');
-				$pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-				$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-				} 
-				catch(Exception $e)
-				{
-				echo "Impossible d'accéder à la base de données SQLite : ".$e->getMessage();
-				die();
-				};
-				
-				$table = $_POST['table'];
-				$file = $_POST['file'];
-				$phrase_sql = "DROP table $table";
-				$preparation = $pdo->prepare($phrase_sql);
-				
-				if ($preparation->execute()) 
-				{
-				unlink('./include/'.$file.'.php');
-				header('Location: index.php#home');
-				} 
-				else 
-				{
-				echo "OOOPS!";
-				}
+			{
+			$pdo = new PDO('sqlite:'.dirname(__DIR__).'/db/home.sqlite');
+			$pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+			$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+			} 
+			catch(Exception $e)
+			{
+			echo "Impossible d'accéder à la base de données SQLite : ".$e->getMessage();
+			die();
+			};
+			
+			$table = $_POST['table'];
+			$file = $_POST['file'];
+			$phrase_sql = "DROP table $table";
+			$preparation = $pdo->prepare($phrase_sql);
+			
+			if ($preparation->execute()) 
+			{
+			unlink('./include/'.$file.'.php');
+			header('Location: index.php#home');
+			} 
+			else 
+			{
+			echo "OOOPS!";
+			}
 						
 		}
 		
--- core/DisplayItems.php
+++ core/DisplayItems.php
@@ -8,57 +8,57 @@
 		
 		{
 			global $tablename;
-			try
-					{
-					$pdo = new PDO('sqlite:'.dirname(__DIR__).'/db/home.sqlite');
-					$pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-					$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-					} 
-					catch(Exception $e)
-					{
-					echo "Impossible d'accéder à la base de données SQLite : ".$e->getMessage();
-					die();
-					};
+				try
+				{
+				$pdo = new PDO('sqlite:'.dirname(__DIR__).'/db/home.sqlite');
+				$pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+				$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+				} 
+				catch(Exception $e)
+				{
+				echo "Impossible d'accéder à la base de données SQLite : ".$e->getMessage();
+				die();
+				};
+			
+				$phrase_sql = "SELECT * FROM $tablename;";
+				$preparation = $pdo->prepare($phrase_sql);
+				$preparation->execute();
+				$data=$preparation->fetchAll( PDO::FETCH_ASSOC );
 				
-					$phrase_sql = "SELECT * FROM $tablename;";
-					$preparation = $pdo->prepare($phrase_sql);
-					$preparation->execute();
-					$data=$preparation->fetchAll( PDO::FETCH_ASSOC );
-					
-					foreach ($data as $data)			
-					{
-					echo '
-					<style>
-					#settings'.$data['modal_name'].':target{
-					display: block;}
-					</style>
-					<div class="content">
-						<a href="#settings'.$data['modal_name'].'" class="settings">&#9776;</a>
-						<a title="'.$data['description'].'" href="http://'.$data['url'].'"><img src="'.$data['img'].'" /></a>
-						<p>'.$data['titre'].'</p>
-					</div>
-					
-					<div class="modalLayer" id="settings'.$data['modal_name'].'">
-			<div class="popup_block">
-				<h1>Update or delete item <a href="#home" class="croix">&#10006;</a></h1>
-					<form method="post" id="content">
-						<label>Titre:</label>
-							<input type="text" name="titre" value="'.$data['titre'].'"><br />
-						<label>Description:</label>
-							<input type="text" name="description" value="'.$data['description'].'"><br />
-						<label>url:</label>
-							<input type="text" name="url" value="'.$data['url'].'"><br />
-						<label>Icone:</label>
-							<input type="text" name="img" value="'.$data['img'].'"><br />
-							<input type="hidden" name="update_item" value="'.$tablename.'">
-							<input type="hidden" name="item_id" value="'.$data['id'].'">
-							<input type="submit" name="update" value="update"/>
-							<input type="submit" name="delete" value="delete"/>
-					</form>
-			</div>
-			</div>
-					';
-					}
+				foreach ($data as $data)			
+				{
+				echo '
+				<style>
+				#settings'.$data['modal_name'].':target{
+				display: block;}
+				</style>
+				<div class="content">
+					<a href="#settings'.$data['modal_name'].'" class="settings">&#9776;</a>
+					<a title="'.$data['description'].'" href="http://'.$data['url'].'"><img src="'.$data['img'].'" /></a>
+					<p>'.$data['titre'].'</p>
+				</div>
+				
+				<div class="modalLayer" id="settings'.$data['modal_name'].'">
+		<div class="popup_block">
+			<h1>Update or delete item <a href="#home" class="croix">&#10006;</a></h1>
+				<form method="post" id="content">
+					<label>Titre:</label>
+						<input type="text" name="titre" value="'.$data['titre'].'"><br />
+					<label>Description:</label>
+						<input type="text" name="description" value="'.$data['description'].'"><br />
+					<label>url:</label>
+						<input type="text" name="url" value="'.$data['url'].'"><br />
+					<label>Icone:</label>
+						<input type="text" name="img" value="'.$data['img'].'"><br />
+						<input type="hidden" name="update_item" value="'.$tablename.'">
+						<input type="hidden" name="item_id" value="'.$data['id'].'">
+						<input type="submit" name="update" value="update"/>
+						<input type="submit" name="delete" value="delete"/>
+				</form>
+		</div>
+		</div>
+				';
+				}
 		
 		}
 		
